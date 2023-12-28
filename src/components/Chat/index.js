2f9d2a167dfa6419412a641f0afaf15c2d427b75
Afroz.Cometchat <afroz.khan@cometchat.com>
2023-12-28 12:37:03 +0530

    added set caption and tags functionlity for media message

--- src/components/Chat/index.js
+++ src/components/Chat/index.js
@@ -5,6 +5,8 @@ const Chat = () => {
     const username = localStorage.getItem("username")
     const [textMessage, setTextMessage] = useState('')
     const [mediaMessage, setMediaMessage] = useState(false)
+    const [mediaCaption, setMediaCaption] = useState('')
+    const [mediaTags, setMediaTags] = useState('');
 
     // send text messages
     const sendMessageTo = (uid) => {
@@ -28,7 +30,9 @@ const Chat = () => {
         let messageType = CometChat.MESSAGE_TYPE.FILE;
         let receiverType = CometChat.RECEIVER_TYPE.USER;
         let mediaMessageInfo = new CometChat.MediaMessage(receiverID, mediaMessage, messageType, receiverType);
-        mediaMessage.setCaption("This is the caption")
+        mediaMessageInfo.setCaption(mediaCaption ? mediaCaption : null);
+        let tags = mediaTags.split(", ")
+        mediaMessageInfo.setTags(tags);
 
         CometChat.sendMediaMessage(mediaMessageInfo).then(
             message => {
@@ -51,17 +55,19 @@ const Chat = () => {
             <input placeholder='Enter your message here' value={textMessage} onChange={(e) => setTextMessage(e.target.value)} />
             <br />
             <p>Send to :-</p>
-            {username == "Captain America" ? '' : <button onClick={() => sendMessageTo("superhero2")}>Captain America</button>}
-            {username == 'Spiderman' ? '' : <button onClick={() => sendMessageTo("superhero3")}>Spiderman</button>}
-            {username == 'Wolverine' ? '' : <button onClick={() => sendMessageTo("superhero4")}>Wolverine</button>}
-            {username == 'Iron Man' ? '' : <button onClick={() => sendMessageTo("superhero1")}>Iron Man</button>}
+            {username === "Captain America" ? '' : <button onClick={() => sendMessageTo("superhero2")}>Captain America</button>}
+            {username === 'Spiderman' ? '' : <button onClick={() => sendMessageTo("superhero3")}>Spiderman</button>}
+            {username === 'Wolverine' ? '' : <button onClick={() => sendMessageTo("superhero4")}>Wolverine</button>}
+            {username === 'Iron Man' ? '' : <button onClick={() => sendMessageTo("superhero1")}>Iron Man</button>}
             <hr color="blue" />
             <input type="file" onChange={(e) => setMediaMessage(e.target.files[0])} />
+            <input placeholder='Caption' type='text' value={mediaCaption} onChange={(e) => setMediaCaption(e.target.value)} />
+            <input type='text' placeholder='Tags' value={mediaTags} onChange={(e) => setMediaTags(e.target.value)} />
             <p>Send to :-</p>
-            {username == "Captain America" ? '' : <button onClick={() => sendMediaMessageTo("superhero2")}>Captain America</button>}
-            {username == 'Spiderman' ? '' : <button onClick={() => sendMediaMessageTo("superhero3")}>Spiderman</button>}
-            {username == 'Wolverine' ? '' : <button onClick={() => sendMediaMessageTo("superhero4")}>Wolverine</button>}
-            {username == 'Iron Man' ? '' : <button onClick={() => sendMediaMessageTo("superhero1")}>Iron Man</button>}
+            {username === "Captain America" ? '' : <button onClick={() => sendMediaMessageTo("superhero2")}>Captain America</button>}
+            {username === 'Spiderman' ? '' : <button onClick={() => sendMediaMessageTo("superhero3")}>Spiderman</button>}
+            {username === 'Wolverine' ? '' : <button onClick={() => sendMediaMessageTo("superhero4")}>Wolverine</button>}
+            {username === 'Iron Man' ? '' : <button onClick={() => sendMediaMessageTo("superhero1")}>Iron Man</button>}
         </div>
     )
 }
